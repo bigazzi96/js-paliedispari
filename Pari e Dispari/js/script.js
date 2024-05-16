a989d9a0bd62c7c5335bfdc7cabc0a6ba69b0bca
bigazzi96 <fbigazzi96@gmail.com>
2024-05-16 22:50:16 +0200

    create constanti

---
+++ Pari e Dispari/js/script.js
@@ -0,0 +1,6 @@
+
+//constante numero utente
+const numeroUtente = parseInt(prompt('Inserisci un numero da 1 a 5'), 10);
+
+//constante numero per computer
+ const numeroComputer = getRandomNumber(1,5);
