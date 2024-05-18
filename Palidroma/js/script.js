5ff055d981f13c48650e43d6174d411a2d8d70b0
bigazzi96 <fbigazzi96@gmail.com>
2024-05-18 13:15:22 +0200

    fixed problems

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -1,14 +1,28 @@
 //funzione per verificare se la parola è palindroma
 
- function invertiParola(parola)){
-    let parolaInversa = '';
-  
-    let i = parola.length - 1;
-  
-    while (i >= 0) {
-      parolaInversa += parola[i];
-      i--;
-    }
-  
-    return parolaInversa;
+let userword = prompt ('type a word').trim().toLowerCase();
+
+while(userword.length < 1){
+  userword=prompt ('type a word').trim().toLowerCase();
+}
+
+ function isPalindrome(word)){
+  if(word.length == 1){
+    return true;
   }
+    else{
+      let reversedWord =''
+      for(let index = word.length - 1; index >= 0; index--){
+        console.log(word[index], reversedWord);
+        reversedWord += word[index];
+   
+    }
+    
+    if(reversedWord === word){
+      return true;
+      } else{
+        return false
+      }
+    }
+  }
+  console.log(IS ${userword},| isPalindrome(userword);
--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -4,3 +4,7 @@ const numeroUtente = parseInt(prompt('Inserisci un numero da 1 a 5'), 10);
 
 //constante numero per computer
  const numeroComputer = getRandomNumber(1,5);
+
+ 
+ const somma = numeroUtente + numeroPc;
+ console.log(somma);
